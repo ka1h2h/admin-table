7eb67eb0b3ad3625fb74f85cab3ec31886699e58
Ilya Ponomarev <97126417+ka1h2h@users.noreply.github.com>
2022-12-14 01:22:53 +0300

    Начало настройки slices
    - создание провайдеров
    - создание лоадеров

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-export default function App() {
+import { useAppSelector } from "./redux/hooks";
+import { UserData } from "./redux/UserSlice";
+
+export const App: React.FC = () => {
+  interface IMix {
+    name: string;
+    id: number;
+    phone: string;
+    email: string;
+    city: string;
+  } // спросить у Паши, есть ли возможность не перечислять каждое поле, ведь их может быть и 100
+
+  const rows = useAppSelector((state) => state.user.content);
   return (
     <div>
-      <div></div>
+      {Object.values(rows).map((item: IMix, index: number) => {
+        return <div>{item.name}</div>;
+      })}
     </div>
   );
-}
+};
---
+++ src/redux/index.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { UserSlice } from "./UserSlice";
+
+export const store = configureStore({
+  reducer: {
+    user: UserSlice.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
